40812c5522d3f21f5e40eb3c798871bbf1ae68fd
Veríssimo Terceiro <terceiroverissimo@gmail.com>
2024-05-30 19:34:03 -0300

    atualiza modelo atleta: relaciona com categorias e centro_treinamento

--- workout_api/atleta/models.py
+++ workout_api/atleta/models.py
@@ -1,10 +1,13 @@
 from datetime import datetime
-from sqlalchemy import DateTime, Float, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from contrib.models import BaseModel
+from categorias.models import CategoriaModel
+from centro_treinamento import CentroTreinamentoModel
 
 class AtletaModel(BaseModel):
     __tablename__ = "atletas"
+    
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     nome: Mapped[str] = mapped_column(String[50], nullable=False)
     cpf: Mapped[str] = mapped_column(String[11], nullable=False)
@@ -14,4 +17,14 @@ class AtletaModel(BaseModel):
     sexo: Mapped[str] = mapped_column(String[1], nullable=False)
     created_at: Mapped[datetime] = mapped_column(
         DateTime, nullable=False, default=datetime.now
+    )
+    categoria: Mapped["CategoriaModel"] = relationship(back_populates="atleta")
+    categoria_id: Mapped[int] = mapped_column(
+        ForeignKey("categorias.pk_id"), nullable=False
+    )
+    centro_treinamento: Mapped["CentroTreinamentoModel"] = relationship(
+        back_populates="atleta", lazy="selectin"
+    )
+    centro_treinamento_id: Mapped[int] = mapped_column(
+        ForeignKey("centros_treinamento.pk_id")
     )
