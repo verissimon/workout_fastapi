a39105ef570f18848ef50cd234d57f202b021ad4
Veríssimo Terceiro <terceiroverissimo@gmail.com>
2024-05-31 01:29:08 -0300

    implementa controller para categoria

--- workout_api/categorias/controller.py
+++ workout_api/categorias/controller.py
@@ -1,5 +1,60 @@
-from fastapi import APIRouter
+from uuid import uuid4
+from fastapi import APIRouter, HTTPException, status, Body
+from pydantic import UUID4
+from sqlalchemy.future import select
+
+from workout_api.categorias.schemas import CategoriaIn, CategoriaOut
 from workout_api.categorias.models import CategoriaModel
+
+from workout_api.contrib.dependencies import DatabaseDependency
 
 router = APIRouter()
 
+
+@router.post("/", summary="Cadastrar categoria", status_code=status.HTTP_201_CREATED)
+async def post(
+    db_session: DatabaseDependency,
+    categoria_in: CategoriaIn = Body(...),
+    response_model=CategoriaOut,
+) -> CategoriaOut:
+    categoria_out = CategoriaOut(id=uuid4(), **categoria_in.model_dump())
+    categoria_model = CategoriaModel(**categoria_out.model_dump())
+    db_session.add(categoria_model)
+    await db_session.commit()
+    return categoria_out
+
+
+@router.get(
+    "/",
+    summary="Listar categorias",
+    status_code=status.HTTP_200_OK,
+    response_model=list[CategoriaOut],
+)
+async def get_all(db_session: DatabaseDependency) -> list[CategoriaOut]:
+    categorias: list[CategoriaOut] = (
+        (await db_session.execute(select(CategoriaModel))).scalars().all()
+    )
+
+    return categorias
+
+
+@router.get(
+    "/{id}",
+    summary="Listar categoria por id",
+    status_code=status.HTTP_200_OK,
+    response_model=CategoriaOut,
+)
+async def get_by_id(db_session: DatabaseDependency, id: UUID4) -> CategoriaOut:
+    categoria: CategoriaOut = (
+        (await db_session.execute(select(CategoriaModel).filter_by(id = id)))
+        .scalars()
+        .first()
+    )
+    
+    if not categoria:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f'Categoria não encontrada no id: {id}'
+        )
+
+    return categoria
--- workout_api/categorias/schemas.py
+++ workout_api/categorias/schemas.py
@@ -1,10 +1,14 @@
 from typing import Annotated
 
-from pydantic import Field
-from contrib.schemas import BaseSchema
+from pydantic import Field, UUID4
+from workout_api.contrib.schemas import BaseSchema
 
 
 class CategoriaIn(BaseSchema):
     nome: Annotated[
-        str, Field(description="Nome da categoria", examples=["Scale"], max_length=10)
+        str, Field(description="Nome da categoria", examples=["Scale"], max_length=21)
     ]
+
+
+class CategoriaOut(CategoriaIn):
+    id: Annotated[UUID4, Field(description="Identificador da categoria")]
